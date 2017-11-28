2bf601701c658b2d328196a1ad75f83986c9840b
Manidhar Vutla <vutlamanidhar@gmail.com>
2017-11-28 15:21:01 +0530

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,27 @@
+<?php
+include("phpmailer/class.phpmailer.php");
+include("phpmailer/class.smtp.php");
+$mail = new PHPMailer(true); // create a new object
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 2; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 25; // or 587
+$mail->IsHTML(true);
+$mail->Username = "vutlamanidhar";
+$mail->Password = "Dhar5199!";
+$mail->From="vutlamanidhar@gmail.com";
+$mail->Subject = "Test";
+$mail->Body = "hello";
+$mail->AddAddress("rahdinam@gmail.com");
+if(!$mail->Send())
+{
+echo "Mailer Error: " . $mail->ErrorInfo;
+}
+else
+{
+echo "Message has been sent";
+}
+?>
